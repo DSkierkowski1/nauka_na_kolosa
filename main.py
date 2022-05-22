ea86f58b363da5e26ffeee00b8986f77febc55a8
Daniel <daniel.skierkowski9803@gmail.com>
2022-05-22 20:50:44 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+## Dobrze rozwiązany kolso
+#zad 1
+# import math
+# from math import *
+# a = math.e**4
+# logarytm = log(35+a)
+# b = pow(logarytm,1/5)
+# print(b)
+#zad 2
+# lista1 = [1,2,3,4,5,6,7]
+# i = len(lista1)
+# lista2 = []
+# for i in range(i):
+#     if i%2 == 0:
+#         lista2.append(lista1[i])
+# else:
+#     i+=1
+# print(lista2)
+# j = len(lista2)
+# a = 0
+# for j in range(j):
+#    print(lista2[j])
+#    a+=lista2[j]
+#    j+=1
+# print(a)
+#zad 3
+import sys
+import random
+# def listy():
+#     a = input('Podaj liczbe a')
+#     a = int(a)
+#     lista1 = []
+#     licznik = 0
+#     while licznik != 10:
+#         b = random.randint(0, 100)
+#         lista1.append(b)
+#         licznik += 1
+#     print(lista1)
+#     lista2 = []
+#     for i in range(len(lista1)):
+#         lista2.append(a + lista1[i])
+#     print(lista2)
+# print(listy())
+#zad 4
+# import random
+# try:
+#     a = input('Podaj liczbę typu intiger a:')
+#     a = int(a)
+#     lista1 = []
+#     licznik = 0
+#     while licznik != 10:
+#         b = random.randint(0, 100)
+#         lista1.append(b)
+#         licznik += 1
+#     print(lista1)
+#     c = 0
+#     for x in range(len(lista1)):
+#         c += lista1[x]
+#     print(c * a)
+# except ValueError:
+#     print("Nie wprowadziłeś ziennej typu int")
